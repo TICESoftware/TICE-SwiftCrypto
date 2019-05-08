af88de56f8fb9e1e1ab64d986dbb366af9de9817
Simon Kempendorf <simon@code28.de>
2019-05-08 14:23:20 +0200

    Update models

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LetsMeetCrypto",
     platforms: [
-        .macOS(.v10_13), .iOS(.v12),
+        .macOS(.v10_14), .iOS(.v12),
     ],
     products: [
         .library(
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["LetsMeetCrypto"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:AnbionApps/letsmeet-models.git", from: "5.0.0"),
+        .package(url: "git@github.com:AnbionApps/letsmeet-models.git", from: "6.0.0"),
         .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.4.1"),
         .package(url: "https://github.com/IBM-Swift/BlueECC.git", from: "1.2.1")
     ],
