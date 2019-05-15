34a93b2a0b6b8977d951f40a168a36efb2cde291
Simon Kempendorf <simon@code28.de>
2019-05-15 20:24:26 +0200

    Add methods to generate and parse auth header

---
+++ Sources/AuthHeaderClaims.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2019 Anbion. All rights reserved.
+//
+
+import Foundation
+import LetsMeetModels
+import SwiftJWT
+
+public struct AuthHeaderClaims: Claims {
+    public let iss: UserId
+    public let iat: Date?
+    public let exp: Date?
+    public let nonce: Data
+}
