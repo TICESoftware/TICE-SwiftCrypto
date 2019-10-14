273eaf4875a213eed46f804e0e44f19caf3e434d
Fabio Tacke <fabio@tacke.berlin>
2019-10-14 16:00:50 +0200

    Update models

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["LetsMeetCrypto"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:AnbionApps/letsmeet-models.git", Version(19,0,0)...Version(26,0,0)),
+        .package(url: "git@github.com:AnbionApps/letsmeet-models.git", Version(19,0,0)...Version(27,0,0)),
         .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.4.1"),
         .package(url: "https://github.com/IBM-Swift/BlueECC.git", from: "1.2.1"),
         .package(url: "git@github.com:AnbionApps/X3DH.git", from: "1.0.0"),
