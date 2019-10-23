0eba001ffa0f056fc067f88acb8cf6432cf184c8
Simon Kempendorf <simon@code28.de>
2019-10-23 14:17:13 +0200

    Update models

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["TICECrypto"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:AnbionApps/tice-models.git", Version(19,0,0)...Version(29,0,0)),
+        .package(url: "git@github.com:AnbionApps/tice-models.git", Version(19,0,0)..<Version(31,0,0)),
         .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.4.1"),
         .package(url: "https://github.com/IBM-Swift/BlueECC.git", from: "1.2.1"),
         .package(url: "git@github.com:AnbionApps/X3DH.git", from: "1.0.0"),
