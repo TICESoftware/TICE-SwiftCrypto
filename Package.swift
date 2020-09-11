16d0f87c769b8708aa464aace6d3cc9d3f3b8767
Simon Kempendorf <simon@code28.de>
2020-09-11 13:06:43 +0200

    Update models

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["TICECrypto"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/TICESoftware/TICE-SwiftModels.git", Version(19,0,0)..<Version(48,0,0)),
+        .package(url: "https://github.com/TICESoftware/TICE-SwiftModels.git", Version(19,0,0)..<Version(49,0,0)),
         .package(url: "https://github.com/TICESoftware/Swift-JWT.git", from: "3.6.2"),
         .package(url: "https://github.com/TICESoftware/BlueECC.git", from: "1.2.6"),
         .package(url: "https://github.com/TICESoftware/X3DH.git", from: "2.0.0"),
