2f9eff292c6d2e3f17cf6873cc69bee3683a50dd
Fabio Tacke <f.tacke@ticeapp.com>
2021-03-16 15:02:57 +0100

    Use Sodium with SPM workaround

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,17 +14,17 @@ let package = Package(
             targets: ["TICECrypto"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/TICESoftware/TICE-SwiftModels.git", Version(49,0,0)..<Version(50,0,0)), 
-        .package(url: "https://github.com/TICESoftware/Swift-JWT.git", from: "3.6.2"),
-        .package(url: "https://github.com/TICESoftware/BlueECC.git", from: "1.2.6"),
-        .package(url: "https://github.com/TICESoftware/X3DH.git", from: "2.0.7"),
-        .package(url: "https://github.com/TICESoftware/DoubleRatchet.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"), 
+        .package(name: "TICEModels", url: "https://github.com/TICESoftware/TICE-SwiftModels.git", from: "49.0.0"),
+        .package(name: "SwiftJWT", url: "https://github.com/TICESoftware/Swift-JWT.git", from: "3.6.2"),
+        .package(name: "CryptorECC", url: "https://github.com/TICESoftware/BlueECC.git", from: "1.2.6"),
+        .package(url: "https://github.com/TICESoftware/X3DH.git", from: "2.0.9"),
+        .package(url: "https://github.com/TICESoftware/DoubleRatchet.git", from: "2.0.2"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "TICECrypto",
-            dependencies: ["TICEModels", "SwiftJWT", "CryptorECC", "X3DH", "DoubleRatchet", "Logging"],
+            dependencies: ["TICEModels", "SwiftJWT", "CryptorECC", "X3DH", "DoubleRatchet", .product(name: "Logging", package: "swift-log")],
             path: "Sources"),
         .testTarget(
             name: "TICECryptoTests",
